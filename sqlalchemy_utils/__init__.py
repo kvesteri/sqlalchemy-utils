135e75d2ddc0dd87c45c2a0bab1a04d8cc795144
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-03-01 16:56:53 +0200

    Updated doc block

--- sqlalchemy_utils/__init__.py
+++ sqlalchemy_utils/__init__.py
@@ -119,9 +119,12 @@ def defer_except(query, columns):
     """
     Deferred loads all columns in given query, except the ones given.
 
+    This function is very useful when working with models with myriad of
+    columns and you want to deferred load many columns.
+
         >>> from sqlalchemy_utils import defer_except
-        >>> query = session.query(User)
-        >>> query = defer_except(User, [User.id, User.name])
+        >>> query = session.query(Article)
+        >>> query = defer_except(Article, [Article.id, Article.name])
 
     :param columns: columns not to deferred load
     """
