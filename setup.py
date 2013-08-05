29ad0343a33b8dfab68646370e8d2d8cd79b728c
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-08-05 13:34:01 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ for name, requirements in extras_require.items():
 
 setup(
     name='SQLAlchemy-Utils',
-    version='0.16.2',
+    version='0.16.3',
     url='https://github.com/kvesteri/sqlalchemy-utils',
     license='BSD',
     author='Konsta Vesterinen',
--- sqlalchemy_utils/__init__.py
+++ sqlalchemy_utils/__init__.py
@@ -34,7 +34,7 @@ from .types import (
 )
 
 
-__version__ = '0.16.2'
+__version__ = '0.16.3'
 
 
 __all__ = (
