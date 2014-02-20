c23b5dc0ae64753b1fdbe4aa501ab193e0c8a805
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-02-20 13:28:06 +0200

    Raise SA dep to 0.9.3

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     ],
     install_requires=[
         'six',
-        'SQLAlchemy>=0.8.0',
+        'SQLAlchemy>=0.9.3',
         'toolz>=0.4.1',
         'total_ordering>=0.1'
         if sys.version_info[0] == 2 and sys.version_info[1] < 7 else ''
