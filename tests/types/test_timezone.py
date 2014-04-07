f1fdb89f299d16fa6f8b1b1ce8e5ff9bf2cfdbc1
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-04-07 23:35:48 +0300

    Fix indentation

--- tests/types/test_timezone.py
+++ tests/types/test_timezone.py
@@ -11,11 +11,14 @@ import pytz
 class TestTimezoneType(TestCase):
     def create_models(self):
         class Visitor(self.Base):
-            __tablename__ = 'document'
+            __tablename__ = 'visitor'
             id = sa.Column(sa.Integer, primary_key=True)
-            timezone_dateutil = sa.Column(timezone.TimezoneType(
-                                          backend='dateutil'))
-            timezone_pytz = sa.Column(timezone.TimezoneType(backend='pytz'))
+            timezone_dateutil = sa.Column(
+                timezone.TimezoneType(backend='dateutil')
+            )
+            timezone_pytz = sa.Column(
+                timezone.TimezoneType(backend='pytz')
+            )
 
             def __repr__(self):
                 return 'Visitor(%r)' % self.id
@@ -32,9 +35,11 @@ class TestTimezoneType(TestCase):
         self.session.commit()
 
         visitor_dateutil = self.session.query(self.Visitor).filter_by(
-            timezone_dateutil='America/Los_Angeles').first()
+            timezone_dateutil=u'America/Los_Angeles'
+        ).first()
         visitor_pytz = self.session.query(self.Visitor).filter_by(
-            timezone_pytz='America/Los_Angeles').first()
+            timezone_pytz=u'America/Los_Angeles'
+        ).first()
 
         assert visitor_dateutil is not None
         assert visitor_pytz is not None
