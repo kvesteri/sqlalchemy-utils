b6af68f89277af841bc9534574c5b94cf260f648
Ryan Leckey <rleckey@concordusapps.com>
2014-07-08 10:49:49 -0700

    Add a 'Timestamp' model that can be added as a mixin to declarative models to provide created and updated properties.

---
+++ sqlalchemy_utils/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import sqlalchemy as sa
+
+
+class Timestamp:
+    """Adds `created` and `updated` columns to a derived declarative model.
+
+    The `created` column is handled through a default and the `updated`
+    column is handled through a `before_update` event that propagates
+    for all derived declarative models.
+
+    ::
+        import sqlalchemy as sa
+        from sqlalchemy_utils import Timestamp
+
+        class SomeModel(Base, Timestamp):
+            __tablename__ = 'somemodel'
+            id = sa.Column(sa.Integer, primary_key=True)
+    """
+
+    created = sa.Column(sa.DateTime, default=datetime.utcnow, nullable=False)
+    updated = sa.Column(sa.DateTime, default=datetime.utcnow, nullable=False)
+
+
+@sa.event.listens_for(Timestamp, 'before_update', propagate=True)
+def timestamp_before_update(mapper, connection, target):
+    # When a model with a timestamp is updated; force update the updated
+    # timestamp.
+    target.updated = datetime.utcnow()
