85170dfb584798b19245459a1f2ea89deef42a09
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2014-12-16 15:41:46 +0200

    Make aggregates fully support column aliases

--- tests/__init__.py
+++ tests/__init__.py
@@ -17,6 +17,10 @@ def count_sql_calls(conn, cursor, statement, parameters, context, executemany):
         conn.query_count += 1
     except AttributeError:
         conn.query_count = 0
+    try:
+        conn.queries.append(statement)
+    except AttributeError:
+        conn.queries = [statement]
 
 
 warnings.simplefilter('error', sa.exc.SAWarning)
@@ -55,6 +59,7 @@ class TestCase(object):
     def teardown_method(self, method):
         aggregates.manager.reset()
         self.session.close_all()
+        self.connection.queries = []
         if self.create_tables:
             self.Base.metadata.drop_all(self.connection)
         self.connection.close()
