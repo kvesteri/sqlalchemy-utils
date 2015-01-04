e15bc1b17d1b6f47df566cea491aba17d5abb583
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2015-01-04 22:12:47 +0200

    Remove unused functions

--- sqlalchemy_utils/functions/defer_except.py
+++ sqlalchemy_utils/functions/defer_except.py
@@ -1,38 +1,6 @@
-import six
 from sqlalchemy import inspect
 from sqlalchemy.orm import defer
 from sqlalchemy.orm.properties import ColumnProperty
-from sqlalchemy.orm.query import _ColumnEntity
-from sqlalchemy.orm.mapper import Mapper
-
-
-def property_names(properties):
-    names = []
-    for property_ in properties:
-        if isinstance(property_, six.string_types):
-            names.append(property_)
-        else:
-            names.append(
-                '%s.%s' % (
-                    property_.class_.__name__,
-                    property_.key
-                )
-            )
-    return names
-
-
-def query_entities(query):
-    entities = []
-    for entity in query._entities:
-        if not isinstance(entity, _ColumnEntity):
-            entities.append(entity.entity_zero.class_)
-
-    for entity in query._join_entities:
-        if isinstance(entity, Mapper):
-            entities.append(entity.class_)
-        else:
-            entities.append(entity)
-    return entities
 
 
 def defer_except(query, columns):
