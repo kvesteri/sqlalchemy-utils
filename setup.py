a7c5b3cb26c040e8e47c21c9cf250eb16bff5299
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2015-07-21 09:20:42 +0300

    Update pytest requirement

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ PY3 = sys.version_info[0] == 3
 
 extras_require = {
     'test': [
-        'pytest==2.3.5',
+        'pytest>=2.7.1',
         'Pygments>=1.2',
         'Jinja2>=2.3',
         'docutils>=0.10',
