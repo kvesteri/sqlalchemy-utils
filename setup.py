f7c6444500b926b994bb4bb099a94629fa5cb9a9
Ryan Grout <rgrout@continuum.io>
2015-10-19 12:45:36 -0500

    sqlalchemy-utils already depends on a version of phonenumbers that is Py3K compatible. (see extra_requires dict).

--- setup.py
+++ setup.py
@@ -74,11 +74,6 @@ setup(
     zip_safe=False,
     include_package_data=True,
     platforms='any',
-    dependency_links=[
-        # 5.6b1 only supports python 3.x / pending release
-        'git+git://github.com/daviddrysdale/python-phonenumbers.git@python3'
-        '#egg=phonenumbers3k-5.6b1',
-    ],
     install_requires=[
         'six',
         'SQLAlchemy>=1.0',
