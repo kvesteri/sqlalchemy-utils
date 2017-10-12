38e76eec8c4fd4aa8b3a87a77ba720817a19a454
GrxE <grex_e@arcor.de>
2017-10-12 21:16:22 +0200

    updates existing translations
    update modified translated record, even if it was not created using flag_modified(), that import was missing in the first commit

--- sqlalchemy_utils/i18n.py
+++ sqlalchemy_utils/i18n.py
@@ -3,6 +3,8 @@ import sqlalchemy as sa
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql.expression import ColumnElement
+from sqlalchemy.orm.attributes import flag_modified
+
 
 from .exceptions import ImproperlyConfigured
 
