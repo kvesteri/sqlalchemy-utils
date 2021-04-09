2b95760d87277f49b930fce86fdab8c20be2e46c
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2021-04-09 13:39:59 +0100

    Fix install_mssql.sh commands

--- .ci/install_mssql.sh
+++ .ci/install_mssql.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f /etc/apt/sources.list.d/microsoft-prod.list ]; then
     curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    sudo curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+    sudo sh -c "curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/prod.list > /etc/apt/sources.list.d/mssql-release.list"
 fi
 
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17 unixodbc
+sudo ACCEPT_EULA=Y apt-get -y install msodbcsql17 unixodbc
