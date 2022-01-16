bdd0378fa2c85660d4c8e94c6c1edef39bcd3168
Eric Atkin <eatkin@certusllc.us>
2022-01-15 18:20:08 -0700

    Import from collections.abc (for python >=3.10)

--- sqlalchemy_utils/functions/orm.py
+++ sqlalchemy_utils/functions/orm.py
@@ -1,4 +1,5 @@
-from collections import OrderedDict, Sequence
+from collections import OrderedDict
+from collections.abc import Sequence
 from functools import partial
 from inspect import isclass
 from operator import attrgetter
