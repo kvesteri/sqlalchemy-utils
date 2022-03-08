62bb6836ec1f9c45e8e5a39400e2c8cddfcfbe17
Kurt McKee <contactme@kurtmckee.org>
2022-03-08 15:01:39 -0800

    Update docs copyright years
    I noticed this while reviewing the documentation and worried that I was either
    
    * Looking at an old version of the documentation, or
    * The project hadn't been updated in a long time
    
    This metadata update asserts copyright and may help confirm the docs are maintained.

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SQLAlchemy-Utils'
-copyright = u'2013, Konsta Vesterinen'
+copyright = u'2013-2022, Konsta Vesterinen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
