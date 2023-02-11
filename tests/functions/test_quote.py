f922afbb00af8d01eb1b124cf3c464d66dfc5454
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2023-02-11 22:43:59 +0200

    feat: make quote tests SA 2 compatible (#689)

--- tests/functions/test_quote.py
+++ tests/functions/test_quote.py
@@ -4,19 +4,16 @@ from sqlalchemy_utils.functions import quote
 
 
 class TestQuote:
-    def test_quote_with_preserved_keyword(self, engine, connection, session):
+    def test_quote_with_preserved_keyword(self, connection, session):
         assert quote(connection, 'order') == '"order"'
         assert quote(session, 'order') == '"order"'
-        assert quote(engine, 'order') == '"order"'
         assert quote(postgresql.dialect(), 'order') == '"order"'
 
     def test_quote_with_non_preserved_keyword(
         self,
-        engine,
         connection,
         session
     ):
         assert quote(connection, 'some_order') == 'some_order'
         assert quote(session, 'some_order') == 'some_order'
-        assert quote(engine, 'some_order') == 'some_order'
         assert quote(postgresql.dialect(), 'some_order') == 'some_order'
